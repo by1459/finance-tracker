f718b27e022561472b0e03a5f3110476f303fb66
Brian <brianyao170@gmail.com>
2026-08-27 23:40:31 -0700

    setup bare backend with uv

--- server/main.py
+++ server/main.py
@@ -1,1 +1,30 @@
-print("hello world")
+from fastapi import FastAPI
+from pydantic import BaseModel
+from pydantic import alias_generators
+from pydantic.alias_generators import to_camel
+from pydantic import ConfigDict
+
+app = FastAPI()
+
+class CamelModel(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+
+class Transaction(CamelModel):
+    transaction_id: str
+    account_id: str
+    amount: int
+    counterparty: str
+    date: int
+    category: str
+
+
+@app.get("/api/get_transaction")
+def get_transaction() -> Transaction:
+    return Transaction(transaction_id="1", account_id="2", amount=20, counterparty="hello", date=2, category="greetings")
+
+def main():
+    print("Hello from server!")
+
+
+if __name__ == "__main__":
+    main()
