a18e6e3a54febcdc1149254eb22006f3cba89a31
Brian <brianyao170@gmail.com>
2026-09-15 21:29:39 -0700

    add plaid link token function

--- server/main.py
+++ server/main.py
@@ -3,10 +3,41 @@ import os
 import plaid
 from fastapi import FastAPI
 from plaid.api import plaid_api
+from plaid.model.country_code import CountryCode
+from plaid.model.item_public_token_exchange_request import (
+    ItemPublicTokenExchangeRequest,
+)
+from plaid.model.link_token_create_request import LinkTokenCreateRequest
+from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
+from plaid.model.products import Products
 from pydantic import BaseModel, ConfigDict, alias_generators
 from pydantic.alias_generators import to_camel
 
 app = FastAPI()
+
+PLAID_CLIENT_ID = os.getenv("PLAID_CLIENT_ID")
+PLAID_SECRET = os.getenv("PLAID_SECRET")
+PLAID_ENV = os.getenv("PLAID_ENV")
+
+USER = "user"
+
+host = plaid.Environment.Sandbox
+if PLAID_ENV == "sandbox":
+    host = plaid.Environment.Sandbox
+if PLAID_ENV == "production":
+    host = plaid.Environment.Production
+
+configuration = plaid.Configuration(
+    host=host,
+    api_key={
+        "clientId": PLAID_CLIENT_ID,
+        "secret": PLAID_SECRET,
+    },
+)
+
+
+api_client = plaid.ApiClient(configuration)
+plaid_client = plaid_api.PlaidApi(api_client)
 
 
 class CamelModel(BaseModel):
@@ -20,6 +51,23 @@ class Transaction(CamelModel):
     counterparty: str
     date: int
     category: str
+
+
+class LinkTokenResponse(CamelModel):
+    link_token: str
+
+
+@app.post("/api/create_link_token")
+def create_link_token() -> LinkTokenResponse:
+    request = LinkTokenCreateRequest(
+        products=[Products("transactions")],
+        client_name="Brian's very cool test app",
+        country_codes=[CountryCode("US")],
+        language="en",
+        user=LinkTokenCreateRequestUser(client_user_id=USER),
+    )
+    response = plaid_client.link_token_create(request)
+    return response.to_dict()
 
 
 @app.get("/api/get_transaction")
