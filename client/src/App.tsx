6e3766ba47c90ee3ef4eea894bc7612b449c047b
Brian <brianyao170@gmail.com>
2026-09-20 21:17:57 -0700

    add link component in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,57 @@
-import { useState } from "react";
-import { TransactionTable } from "./components/transactions.tsx";
-import type { Transaction } from "./types";
+// import { useState } from "react";
+import type { PlaidLinkOnSuccessMetadata } from "react-plaid-link";
 
-export function App() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  async function getTransactions() {
-    const res = await fetch("/api/get_transaction");
-    const data = await res.json();
-    setTransactions([data]);
-  }
-
-  return (
-    <>
-      <button onClick={getTransactions}>Load</button>
-      <TransactionTable transactions={transactions} />
-    </>
-  );
+// APP COMPONENT
+// Upon rendering of App component, make a request to create and
+// obtain a link token to be used in the Link component
+import React, { useEffect, useState } from "react";
+import { usePlaidLink } from "react-plaid-link";
+export const App = () => {
+  const [linkToken, setLinkToken] = useState(null);
+  const generateToken = async () => {
+    const response = await fetch("/api/create_link_token", {
+      method: "POST",
+    });
+    const data = await response.json();
+    setLinkToken(data.linkToken);
+  };
+  useEffect(() => {
+    generateToken();
+  }, []);
+  return linkToken != null ? <Link linkToken={linkToken} /> : <></>;
+};
+// LINK COMPONENT
+// Use Plaid Link and pass link token and onSuccess function
+// in configuration to initialize Plaid Link
+interface LinkProps {
+  linkToken: string | null;
 }
+const Link: React.FC<LinkProps> = (props: LinkProps) => {
+  const onSuccess = React.useCallback(
+    async (
+      public_token: string | null,
+      metadata: PlaidLinkOnSuccessMetadata,
+    ) => {
+      // send public_token to server
+      const response = await fetch("/api/exchange_public_token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ public_token }),
+      });
+      const data = await response.json();
+    },
+    [],
+  );
+  const config: Parameters<typeof usePlaidLink>[0] = {
+    token: props.linkToken!,
+    onSuccess,
+  };
+  const { open, ready } = usePlaidLink(config);
+  return (
+    <button onClick={() => open()} disabled={!ready}>
+      Link account
+    </button>
+  );
+};
